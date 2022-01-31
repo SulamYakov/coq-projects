64f42c24bb889c5ac133fddaf38cb18b38578046
Yaakov Vainshtein <weinsteinjw31@gmail.com>
2022-01-31 23:26:14 +0200

    Completed proof of 1_2_1d, made progress towards a proof of 1_2_1g

--- topology/Mendelson/Ch1.v
+++ topology/Mendelson/Ch1.v
@@ -11,7 +11,7 @@ Proof.
 	intros.
 	apply Definition_of_Power_set.
 	unfold Included.
-	intuition. (* p -> p is true for any prop p *)
+	trivial. (* p -> p is true for any prop p *)
 Qed.
 
 (* exercise 1_2_1b is false *)
@@ -22,7 +22,9 @@ Theorem ex_1_2_1d : forall (U:Type) (A:Ensemble U),
 Proof.
   intros.
   apply Definition_of_Power_set.
-  intuition. (* empty set is a subset of any set *)
+  unfold Included.
+  intros.
+  inversion H. (* empty set is a subset of any set *)
 Qed.
 
 (* exercise 1_2_1e is true  *)
@@ -31,7 +33,11 @@ Qed.
 Theorem ex_1_2_1g : forall (U:Type) (A B:Ensemble U), 
   Included U A B -> Included (Ensemble U) (Power_set U A) (Power_set U B).
 Proof.
-  intros.
+  unfold Included.
+  intros U A B h1 C.
+  unfold In.
+  intro h2. apply Definition_of_Power_set.
+  unfold Included.
 Admitted.
 
 End Ch1.
